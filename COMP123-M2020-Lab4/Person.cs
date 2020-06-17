2b7669896e2570c3795b6b843f0b1c00131800d0
Wooram Moon <wpdl555@naver.com>
2020-06-17 13:21:52 -0400

    Add project files.

---
+++ COMP123-M2020-Lab4/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lab4
+{
+    class Person
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // CONSTRUCTOR(S)
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} syas Hello!");
+        }
+
+    }
+}
---
+++ COMP123-M2020-Lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate object of type Person
+            Person tom = new Person(name: "Tom", age: 40);
+
+            // use saysHello method with . accessor
+            tom.SaysHello();
+
+
+            Console.ReadLine();
+        }
+    }
+}
